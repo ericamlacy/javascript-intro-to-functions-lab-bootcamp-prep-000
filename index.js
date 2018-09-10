c437d941742c5f8f80734632c5dbe43375bd37dc
Erica Lacy <ericalacy7@gmail.com>
2018-09-10 01:45:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function whisper(string){
 function logShout(string){
   console.log(string.toUpperCase())
 }
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
 function sayHiToGrandma(string){
   if (string.toLowerCase() === lowercase){
     return "I can't hear you!";
